79ef70173f623bf02954d3c745cfe388b892f356
Caio Santos <caiosantosfreitas@hotmail.com>
2020-04-14 19:02:04 -0300

    Validações na API

--- api/src/index.js
+++ api/src/index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 const routes = require('./routes')
 
-<<<<<<< HEAD
 const app = express()   
-=======
-const app = express()
->>>>>>> 97dec7a12312d4bd95368c784cc7e6ffbcc145f4
 
 app.use(cors());
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 app.listen(3333)
